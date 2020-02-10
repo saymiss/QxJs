1d09e86b682c39964cecb44e00ffde4f8010369d
panxsn <48349168+panxsn@users.noreply.github.com>
2020-02-10 23:27:05 +0800

    Add files via upload

---
+++ MolyCam_VaporCam.js
@@ -0,0 +1,24 @@
+/*
+by 蔠點丶 
+
+[rewrite_local]
+# MolyCam
+^https:\/\/pay\.wecut\.com\/apple\/iosVerifyReceipt\.php url script-response-body panxsn/MolyCam_VaporCam.js
+
+[mitm]  
+pay.wecut.com
+*/
+
+var obj = JSON.parse($response.body);
+obj=
+{
+ "msg": "处理成功",
+ "data": {
+  "isValid": 1,
+  "expiresTs": 9876543210
+ },
+ "code": 0
+};
+$done({body: JSON.stringify(obj)});
+
+
