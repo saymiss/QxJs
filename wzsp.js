530630d570338641189e70be2be6093467416742
蔠點丶 <48349168+panxsn@users.noreply.github.com>
2020-02-13 13:06:45 +0800

    Update wzsp.js
    增加APP下载地址

--- wzsp.js
+++ wzsp.js
@@ -1,13 +1,13 @@
 /*
 by 蔠點丶 
+链接：http://t.cn/AiN0mk0G
 
 [rewrite_local]
-# 文字视频
+# 文字视频（开启规则后需重新微信登录）
 ^https:\/\/a\.aipike\.net\/api\/v1\/oauth\/WECHAT url script-response-body panxsn/wzsp.js
 
 [mitm]  
 a.aipike.net
-
 */
 var body = $response.body;
 var url = $request.url;
