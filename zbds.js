3a1320b149247b1ec5ef00eb7296ebfcf0615b31
蔠點丶 <48349168+panxsn@users.noreply.github.com>
2020-02-13 16:41:45 +0800

    Create zbds.js
    装裱大师调试

---
+++ zbds.js
@@ -0,0 +1,20 @@
+/*
+by 蔠點丶 
+
+[rewrite_local]
+# 装裱大师
+^http:\/\/140\.143\.148\.31:7070\/api\/v1\/user\/info url script-response-body panxsn/zbds.js
+
+[mitm]  
+140.143.148.31:7070
+*/
+
+var body = $response.body;
+var url = $request.url;
+var obj = JSON.parse(body);
+
+obj.result.viptime = "9876543210";
+
+body = JSON.stringify(obj);
+
+$done({body});
