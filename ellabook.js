0656fc7ed44968f10936f64d9c780ec5744c05e8
蔠點丶 <48349168+panxsn@users.noreply.github.com>
2020-02-13 18:48:58 +0800

    Update and rename YiLaKanShu.js to ellabook.js

--- ellabook.js
+++ ellabook.js
@@ -1,13 +1,12 @@
 /*
-by 蔠點丶 
+unlock vip by panxsn
 
 [rewrite_local]
 # 咿啦看书
-^https:\/\/api\.ellabook\.cn\/rest\/api\/service url script-response-body panxsn/YiLaKanShu.js
+^https:\/\/api\.ellabook\.cn\/rest\/api\/service url script-response-body panxsn/ellabook.js
 
 [mitm]  
 api.ellabook.cn
-
 */
 
 body = $response.body.replace(/ip":"NO"/g, "ip\":\"YES\"").replace(/ip":"VIP_NO"/g, "ip\":\"VIP_YES\"").replace(/code":"\d{10}/g, "code\":\"0x00000000").replace(/status":"\d/g, "status\":\"1");
