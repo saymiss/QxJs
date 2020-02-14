d007140b4a88aeca1ed3f2c2cefc0d87a23a4b5a
蔠點丶 <48349168+panxsn@users.noreply.github.com>
2020-02-14 12:28:08 +0800

    Create ipmph.js
    人卫临床助手

---
+++ ipmph.js
@@ -0,0 +1,22 @@
+/*
+unlock paid ebook by panxsn
+
+[rewrite_local]
+# 人卫临床助手-解锁付费电子书
+^https:\/\/ccdas\.ipmph\.com\/App\/Pmph\/AppEbookDetail url script-response-body panxsn/ipmph.js
+
+[mitm]  
+ccdas.ipmph.com
+*/
+
+var body = $response.body;
+var url = $request.url;
+var obj = JSON.parse(body);
+
+obj.result.isPay = "Y";
+obj.result.eBookPaygm.vippay = "0.0";
+obj.result.eBookPaygm.pay = "0.0";
+
+body = JSON.stringify(obj);
+
+$done({body});
