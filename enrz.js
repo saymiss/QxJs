a1862e79986fc7531b6fec2774e8e925ed7c7b49
蔠點丶 <48349168+panxsn@users.noreply.github.com>
2020-02-18 12:22:31 +0800

    男人装

---
+++ enrz.js
@@ -0,0 +1,20 @@
+/*
+unlock vip by panxsn
+
+链接：http://t.cn/AilGVWcb
+版本：Appstore v2.4.7
+
+[rewrite_local]
+# 男人装
+^http:\/\/ssl4xyy\.enrz\.com\/api\/user4app\/myinfo url script-response-body panxsn/enrz.js
+
+[mitm]
+ssl4xyy.enrz.com
+*/
+
+var obj = JSON.parse($response.body);
+
+obj.result.IsVip = 1;
+obj.result.ExpTime = "/Date(9876543210000)/";
+
+$done({body: JSON.stringify(obj)});
