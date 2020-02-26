916787bdff2e447c3d2a85cd10c1e3aace6f0ee7
蔠點丶 <48349168+panxsn@users.noreply.github.com>
2020-02-26 15:46:54 +0800

    云梦影视

--- ymys.js
+++ ymys.js
@@ -5,20 +5,15 @@ unlock vip by panxsn
 
 [rewrite_local]
 # 云梦影视
-^https:\/\/v\.dzdv\.cn\/\/api\.php\/provide\/app\/ url script-response-body panxsn/ymys.js
+^https:\/\/v\.dzdv\.cn\/\/api\.php\/provide\/app\/\?ac=login url script-response-body panxsn/ymys.js
 
 [mitm]
 v.dzdv.cn
 */
 
-var url = $request.url;
 var obj = JSON.parse($response.body);
 
-const path = "login";
-
-if (url.indexOf(path) != -1){
 obj.user_end_time = 9876543210;
 obj.user_points = 88888;
-}
 
 $done({body: JSON.stringify(obj)});
