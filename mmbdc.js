98ebc7f6badbe7ef585a42c951ef406096bcdeec
蔠點丶 <48349168+panxsn@users.noreply.github.com>
2020-02-26 16:09:09 +0800

    墨墨背单词 v3.5.4

---
+++ mmbdc.js
@@ -0,0 +1,21 @@
+/*
+by panxsn
+
+[rewrite_local]
+# 墨墨背单词 v3.5.4（旧版）
+^https:\/\/api\.maimemo\.com\/api\/v1\/users\/info url script-response-body panxsn/mmbdc.js
+# 去除系统检测
+^https:\/\/api\.maimemo\.com\/api\/v1\/system\/check url reject
+
+[mitm]
+api.maimemo.com
+*/
+
+var obj = JSON.parse($response.body);
+
+obj.data.user.inf_level = "66";
+obj.data.user.level = "66";
+obj.data.user.inf_pk_level = "66";
+obj.data.user.inf_words_limit = "888888";
+
+$done({body: JSON.stringify(obj)});
