cb333a7a2b07fde24d0a949266453bc44c0e87c1
蔠點丶 <48349168+panxsn@users.noreply.github.com>
2020-03-02 17:26:07 +0800

    闪电下载

---
+++ flashdown.js
@@ -0,0 +1,18 @@
+/*
+unlock vip by panxsn
+
+链接：http://bbs.flashdown365.com/download.html
+
+[rewrite_local]
+# 闪电下载
+^http:\/\/app\.flashdown365\.com\/ios\/login url script-response-body panxsn/flashdown.js
+
+[mitm]
+app.flashdown365.com
+*/
+
+var obj = JSON.parse($response.body);
+
+obj.body.isvip = true;
+
+$done({body: JSON.stringify(obj)});
