854ef32cacd3b5a7b04f9a2d4c8030da2b45185a
蔠點丶 <48349168+panxsn@users.noreply.github.com>
2020-03-26 11:29:27 +0800

    克拉壁纸 v4.7版本可用

--- claritywallpaper.js
+++ claritywallpaper.js
@@ -2,7 +2,7 @@
 unlock pro by panxsn
 
 [rewrite_local]
-# 克拉壁纸Pro
+# 克拉壁纸Pro v4.7（旧版）
 ^https:\/\/claritywallpaper\.com\/clarity\/api\/special url script-response-body panxsn/claritywallpaper.js
 
 [mitm]
