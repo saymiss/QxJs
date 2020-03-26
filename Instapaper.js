889532e4a3e80be172596860d625f89b3aafdf39
蔠點丶 <48349168+panxsn@users.noreply.github.com>
2020-03-26 11:22:50 +0800

    Instapaper

---
+++ Instapaper.js
@@ -0,0 +1,17 @@
+/*
+by panxsn
+
+链接：https://apps.apple.com/cn/app/instapaper/id288545208
+版本：7.8.2
+
+[rewrite_local]
+# Instapaper
+^https:\/\/www\.instapaper\.com\/api\/subscription_status url script-response-body panxsn/Instapaper.js
+
+[mitm]
+www.instapaper.com
+*/
+
+body = $response.body.replace(/.+/, "ipsubnow=1585151004&ipsubexp=2000000000&ipsub=1");
+
+$done({body});
