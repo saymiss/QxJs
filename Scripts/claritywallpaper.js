d843cd7f359bd7ed73a80b5a5e1569ff39592bba
蔠點丶 <48349168+saymiss@users.noreply.github.com>
2021-01-29 20:52:57 +0800

    Update and rename claritywallpaper.js to Scripts/claritywallpaper.js

--- Scripts/claritywallpaper.js
+++ Scripts/claritywallpaper.js
@@ -1,9 +1,9 @@
 /*
-unlock pro by panxsn
+unlock pro by saymiss
 
 [rewrite_local]
 # 克拉壁纸Pro v4.7.4（旧版）
-^https:\/\/claritywallpaper\.com\/clarity\/api\/special url script-response-body panxsn/claritywallpaper.js
+^https:\/\/claritywallpaper\.com\/clarity\/api\/special url script-response-body saymiss/claritywallpaper.js
 
 [mitm]
 claritywallpaper.com
