8efcedbcf32aa1deb2f07090deb0c887516343fb
蔠點丶 <48349168+saymiss@users.noreply.github.com>
2021-01-29 20:53:50 +0800

    Update and rename enrz.js to Scripts/enrz.js

--- Scripts/enrz.js
+++ Scripts/enrz.js
@@ -1,12 +1,12 @@
 /*
-unlock vip by panxsn
+unlock vip by saymiss
 
 链接：http://t.cn/AilGVWcb
 版本：Appstore v2.4.7
 
 [rewrite_local]
 # 男人装
-^http:\/\/ssl4xyy\.enrz\.com\/api\/user4app\/myinfo url script-response-body panxsn/enrz.js
+^http:\/\/ssl4xyy\.enrz\.com\/api\/user4app\/myinfo url script-response-body saymiss/enrz.js
 
 [mitm]
 ssl4xyy.enrz.com
