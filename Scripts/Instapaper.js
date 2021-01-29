962d7d444e376f21ca6ecd51d0ecceeb26d0a8e3
蔠點丶 <48349168+saymiss@users.noreply.github.com>
2021-01-29 20:42:37 +0800

    Update Instapaper.js

--- Scripts/Instapaper.js
+++ Scripts/Instapaper.js
@@ -1,12 +1,12 @@
 /*
-by panxsn
+by saymiss
 
 链接：https://apps.apple.com/cn/app/instapaper/id288545208
 版本：7.8.2
 
 [rewrite_local]
 # Instapaper
-^https:\/\/www\.instapaper\.com\/api\/subscription_status url script-response-body panxsn/Instapaper.js
+^https:\/\/www\.instapaper\.com\/api\/subscription_status url script-response-body saymiss/Instapaper.js
 
 [mitm]
 www.instapaper.com
