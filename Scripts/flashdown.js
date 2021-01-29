b651445b54ef86c478aeba7c2fac8bca3989cc0d
蔠點丶 <48349168+saymiss@users.noreply.github.com>
2021-01-29 20:54:56 +0800

    Update and rename flashdown.js to Scripts/flashdown.js

--- Scripts/flashdown.js
+++ Scripts/flashdown.js
@@ -1,11 +1,11 @@
 /*
-unlock vip by panxsn
+unlock vip by saymiss
 
 链接：http://bbs.flashdown365.com/download.html
 
 [rewrite_local]
 # 闪电下载
-^http:\/\/app\.flashdown365\.com\/ios\/login url script-response-body panxsn/flashdown.js
+^http:\/\/app\.flashdown365\.com\/ios\/login url script-response-body saymiss/flashdown.js
 
 [mitm]
 app.flashdown365.com
