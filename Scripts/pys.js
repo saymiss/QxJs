a6a37ec212bd1d7124ab45d3be4a109c96c5a38b
蔠點丶 <48349168+saymiss@users.noreply.github.com>
2021-01-29 20:58:27 +0800

    Update and rename pys.js to Scripts/pys.js

--- Scripts/pys.js
+++ Scripts/pys.js
@@ -1,12 +1,12 @@
 /*
-unlock vip by panxsn
+unlock vip by saymiss
 
 链接：http://mrw.so/5eT2U5
 版本：Appstore v3.1.9
 
 [rewrite_local]
 # 配音师-文字转语音助手
-^http:\/\/101\.37\.76\.151:8055\/Svip\/SVIP_Existence\.aspx url script-response-body panxsn/pys.js
+^http:\/\/101\.37\.76\.151:8055\/Svip\/SVIP_Existence\.aspx url script-response-body saymiss/pys.js
 
 [mitm]
 101.37.76.151:8055
