8890f1cdb94be21335b2b046ee4bb8752fec641e
蔠點丶 <48349168+saymiss@users.noreply.github.com>
2021-01-29 21:07:18 +0800

    Update and rename zbds.js to Scripts/zbds.js

--- Scripts/zbds.js
+++ Scripts/zbds.js
@@ -1,9 +1,9 @@
 /*
-unlock vip by panxsn
+unlock vip by saymiss
 
 [rewrite_local]
 # 装裱大师
-^http:\/\/140\.143\.148\.31:7070\/api\/v1\/user\/info url script-response-body panxsn/zbds.js
+^http:\/\/140\.143\.148\.31:7070\/api\/v1\/user\/info url script-response-body saymiss/zbds.js
 
 [mitm]  
 140.143.148.31:7070
